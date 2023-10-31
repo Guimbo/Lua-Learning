8c87f691e1ec49255d3b6817e14c97640b79f099
Guilherme Araujo <guiaszul@gmail.com>
2023-10-31 01:35:35 -0300

    Hello Lua para o Cesinha

---
+++ caesar.lua
@@ -0,0 +1,21 @@
+local function encrypt(text,key)
+    return text:gsub("%a", function(t)
+        local base = (t:lower() == t and string.byte('a') or string.byte('A'))
+        local r = t:byte() - base
+        r = r + key
+        r = r%26
+        r = r + base
+        return string.char(r)
+    end)
+end
+
+caesar = {
+    encrypt = encrypt
+}
+
+do 
+    local text = "HACKtoberfest"
+    local encrypted = caesar.encrypt(text,23)
+    print("Texto original: ", text)
+    print("Texto encriptado: ", encrypted)
+end
